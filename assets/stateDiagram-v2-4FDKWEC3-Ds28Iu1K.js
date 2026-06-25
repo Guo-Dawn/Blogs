e9c22de85c85dd97c103377718d49c2d67b6a458
guosijian <19133126876@163.com>
2026-06-25 19:51:46 +0800

    feat:更新V2版本

---
+++ assets/stateDiagram-v2-4FDKWEC3-Ds28Iu1K.js
@@ -0,0 +1,1 @@
+import{s as t,b as r,a,S as s}from"./chunk-DI55MBZ5-CX4GaSXO.js";import{_ as i}from"./mermaid-Bqm_mEET.js";import"./chunk-55IACEB6-C-Uq8atW.js";import"./chunk-QN33PNHL-D7EHiMCl.js";var l={parser:a,get db(){return new s(2)},renderer:r,styles:t,init:i(e=>{e.state||(e.state={}),e.state.arrowMarkerAbsolute=e.arrowMarkerAbsolute},"init")};export{l as diagram};
